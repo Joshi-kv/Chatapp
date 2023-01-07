7de70d74170266118596c3e02d1c00c222fada0f
JOshi kv <joshikv3705@gmail.com>
2023-01-07 18:58:41 +0530

    Message function created and tkinter window created

--- Chat Bot/server.py
+++ Chat Bot/server.py
@@ -1,6 +1,34 @@
 #import socket module for establishing socket 
 import socket
 
+#import tkinter module.
+from tkinter import *
+
+#function to send message
+def send(messagebox,entry):
+    #sendiing message to client
+    message=entry.get()
+    messagebox.insert('end','Server : '+message)
+    entry.delete(0,END)  
+    client.send(bytes(message,'utf-8'))
+    
+#function for receiving message
+def receive(messagebox):
+     message_from_client=client.recv(100)
+     messagebox.insert('end','Client : '+message_from_client.decode('utf-8'))
+         
+root=Tk()
+root.title('Server')
+entry=Entry()
+entry.pack(side=BOTTOM)
+messagebox=Listbox(root,width=100)
+messagebox.pack()
+send_button=Button(root,text="Send",command=lambda:send(messagebox,entry))
+send_button.pack(side=BOTTOM)
+receive_button=Button(root,text="Receive",command=lambda:receive(messagebox))
+receive_button.pack(side=BOTTOM)
+
+
 #establishing socket connection
 server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
@@ -14,10 +42,4 @@ server.listen(4)
 
 #accepting client ip address
 client,address=server.accept()
-while True:
-    #sendiing message to client
-    message=input('Server : ')
-    client.send(bytes(message,'utf-8'))
-    #receiving message from client.
-    message_from_client=client.recv(100)
-    print("Client : ",message_from_client.decode('utf-8'))
+root.mainloop()
